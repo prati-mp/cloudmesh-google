8979422af9f735a636bd0fec648bc9b703cd720e
rahul <software.engr@gmail.com>
2020-04-19 18:39:35 -0700

    Added script to run pytest for Google Compute.

---
+++ tests/test_google_compute.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+options=("-v")
+options+=($1)
+
+echo "Launch pytest ${options[@]}"
+#echo "Set cloud to Google"
+cms set cloud=google
+
+pytest ${options[@]} ../cloudmesh-cloud/tests/cloud/test_00_sys.py
+pytest ${options[@]} ../cloudmesh-cloud/tests/cloud/test_01_clean_local_remote.py
+pytest ${options[@]} ../cloudmesh-cloud/tests/cloud/test_02_key.py
+pytest ${options[@]} ../cloudmesh-cloud/tests/cloud/test_04_flavor.py
+pytest ${options[@]} ../cloudmesh-cloud/tests/cloud/test_05_image.py
+pytest ${options[@]} ../cloudmesh-cloud/tests/cloud/test_08_vm_provider.py
+pytest ${options[@]} ../cloudmesh-cloud/tests/cloud/test_09_cm_names_find.py
